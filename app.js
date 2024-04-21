c4f4e95dabea9a18f3313484bef478782036308f
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 01:58:27 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // write code here
+ <script>
+        let num1 = parseFloat(prompt("ระบุตัวเลขตัวที่1:"));
+        let num2 = parseFloat(prompt("ระบุตัวเลขตัวที่2:"));
+        let num3 = parseFloat(prompt("ระบุตัวเลขตัวที่3:"));
+        
+        let numbers = [num1, num2, num3];
+       
+        numbers.sort(function(a, b) {
+        return b - a;
+        });
+
+        alert("เรียงจากมากไปน้อย: " + numbers.join(", "));
+
+</script>
